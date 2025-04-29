f957cc0352e4eb5b29e8fc749efdf0ba6f02242e
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-04-29 12:08:38 +0600

    info deposite withdrow

---
+++ bank.py
@@ -0,0 +1,34 @@
+class Accoount:
+    listOfAccounts = []
+
+    def __init__(self,name,accountNumber,password,type):
+        self.name = name
+        self.accountNumber = accountNumber
+        self.password = password
+        self.type = type
+
+        self.balance = 0
+        Accoount.listOfAccounts.append(self)
+
+    def changeInformation(self,newName):
+        self.newName = newName
+
+    def changeInformation(self,name,password):
+        self.name = name
+        self.password = password
+
+    def depositeAmount(self,amount):
+        if amount > 0:
+            self.balance += amount
+        else:
+            print("Faild to deposite!")
+    
+    def withdrowAmount(self,amount):
+        if amount > 0 and amount < self.balance:
+            self.balance -= amount
+        else:
+            print("Faild to withdrow!")
+
+
+myAccount = Accoount("Tasnim Rahman",100012,1234,"savings")
+
