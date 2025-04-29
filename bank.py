541f95b2ca911ded19a14440c415210f3bdfa51e
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-04-29 12:16:05 +0600

    inheritance

--- bank.py
+++ bank.py
@@ -1,3 +1,5 @@
+from abc import ABC,abstractmethod
+
 class Accoount:
     listOfAccounts = []
 
@@ -29,6 +31,25 @@ class Accoount:
         else:
             print("Faild to withdrow!")
 
+    def getBalance(self):
+        return self.balance
+    
+    @abstractmethod
+    def showInformation(self):
+        pass
+
+
+class SavingsAccount(Accoount):
+    def __init__(self, name, accountNumber, password, type, intrestRate):
+        self.intrestRate = intrestRate
+        super().__init__(name, accountNumber, password, type)
+
+
+
+
 
 myAccount = Accoount("Tasnim Rahman",100012,1234,"savings")
+
+myAccount.depositeAmount(1000)
+print(myAccount.getBalance())
 
