25b50546e9e83820ed1c7d3e2a517e847c05ab95
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-05-23 12:01:23 +0600

    done

--- number.py
+++ number.py
@@ -1,6 +1,7 @@
 import random
 
 while(True):
+    print("\n")
     print("Welcome To The Game!!")
     print("1.Start A New Game!!")
     print("2. Exit!!")
@@ -11,9 +12,14 @@ while(True):
 
         if number == random_num:
             print("Congo!! You Win!")
+            print("\n")
         else:
             print("You Loss!!")
+            print("\n")
     elif choice == 2:
         print("Thank You!!")
+        print("\n")
+        break
     else:
         print("Invalid Choice!!")
+        print("\n")
